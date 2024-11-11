ecc07b36b9769aa4d16bcbaa5ee11b7da2732f63
MauroJRamos <mauroramosti@gmail.com>
2024-11-11 14:05:29 -0300

    Teste Pauta

--- assembleia/src/test/java/com/votacao/assembleia/model/PautaTest.java
+++ assembleia/src/test/java/com/votacao/assembleia/model/PautaTest.java
@@ -1,5 +1,48 @@
 package com.votacao.assembleia.model;
 
+import com.votacao.assembleia.enums.StatusSessao;
+import com.votacao.assembleia.service.PautaService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.when;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 public class PautaTest {
-    
+
+    @Mock
+    private PautaService pautaService;
+
+    @InjectMocks
+    private Pauta pauta;
+
+    @Test
+    void shouldCreatePauta() {
+        pauta.setTitulo("Exemplo de Pauta");
+        pauta.setDescricao("Descrição da pauta de exemplo");
+
+        assertEquals("Exemplo de Pauta", pauta.getTitulo());
+        assertEquals("Descrição da pauta de exemplo", pauta.getDescricao());
+        assertEquals(StatusSessao.NAO_INICIADA, pauta.getStatusSessao());
+        assertNull(pauta.getInicioSessao());
+        assertNull(pauta.getFimSessao());
+    }
+
+    @Test
+    void shouldOpenSession() {
+        when(pautaService.abrirSessao(1L, 10)).thenReturn(pauta);
+
+        pautaService.abrirSessao(1L, 10);
+
+        assertEquals(StatusSessao.NAO_INICIADA, pauta.getStatusSessao());
+        assertNull(pauta.getInicioSessao());
+        assertNull(pauta.getFimSessao());
+
+    }
+
 }
